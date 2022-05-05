9f808e759b7f05d1ade4f4c83d2b91294ff90ddd
Sasshka <sasshka@LAPTOP-2J4311QG.Dlink>
2022-05-05 15:08:04 +0300

    Some hello world app

--- graph_builder.cpp
+++ graph_builder.cpp
@@ -1,9 +1,11 @@
-#include <gtkmm.h>
+#include "helloworld.h"
+#include "helloworld.cpp"
+#include <gtkmm/application.h>
 
 int main(int argc, char * argv[]) {
     Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
     
-    Gtk::ApplicationWindow window;
+    HelloWorld helloworld;
 
-    return app->run(window);
+    return app->run(helloworld);
 }
