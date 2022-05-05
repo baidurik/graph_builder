74e7422ea8d2d7b85104930ece1da44edaf77582
Sasshka <sasshka@LAPTOP-2J4311QG.Dlink>
2022-05-05 14:08:15 +0300

    fixed gtkmm library

--- graph_builder.cpp
+++ graph_builder.cpp
@@ -1,1 +1,9 @@
-#include <gtk/gtk.h>
+#include <gtkmm.h>
+
+int main(int argc, char * argv[]) {
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+    
+    Gtk::ApplicationWindow window;
+
+    return app->run(window);
+}
